090ab3b17e9e52d8ccddba83db58eaf47e67a079
Jonathan Raine <jonatannnraineeee@gmail.com>
2023-02-05 19:45:32 +0700

    fix

---
+++ lib/class/host.dart
@@ -0,0 +1,17 @@
+class Host {
+  /*
+  -- TO RUN PROJECT IN EMULATOR: Please use the IP below as localhost by giving it an uncommand.
+  -- Make sure to choose just one IP as your host to avoid error.
+  */
+
+  // static const String main = "http://10.0.2.2:3000";
+
+  /*
+  -- TO RUN PROJECT IN USB/LAN DEBUGGING: Please use the IP below and change it to your current Ipv4 address by giving it an uncommand.
+  -- TO GET YOUR Ipv4 ADDRESS: Makesure you are connected to the same LAN with your debug device. Type in 'ipconfig' in cmd
+     (command prompt) and copy the Ipv4 from the debug LAN adapter and paste it to the string down below.
+  -- Make sure to choose just one IP as your host to avoid error.
+  */
+
+  static const String main = "http://192.168.1.8:3000";
+}
